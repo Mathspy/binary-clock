31b32564f06211df814139a72c7820dac3c4a494
Mathspy <mathspy257@gmail.com>
2017-08-01 23:23:58 +0200

    Initial functionality, test covered

--- src/scripts/__test__/timeInBinary.test.js
+++ src/scripts/__test__/timeInBinary.test.js
@@ -10,13 +10,13 @@ describe("timeInBinary", () => {
 
 	test("if given time, returns [hh, mm, ss]", () => {
 		let its5 = moment("05:35:07", "HH:mm:ss")
-		expect(timeInBinary(its5)).toEqual([["0", "5"], ["3", "5"], ["0", "7"]])
+		expect(timeInBinary(its5)).toEqual([["00", "0101"], ["011", "0101"], ["000", "0111"]])
 
 		let timegoesfast = moment("23:59:59", "HH:mm:ss")
-		expect(timeInBinary(timegoesfast)).toEqual([["2", "3"], ["5", "9"], ["5", "9"]])
+		expect(timeInBinary(timegoesfast)).toEqual([["10", "0011"], ["101", "1001"], ["101", "1001"]])
 
 		let goodwork = moment("00:00:00", "HH:mm:ss")
-		expect(timeInBinary(goodwork)).toEqual([["0", "0"], ["0", "0"], ["0", "0"]])
+		expect(timeInBinary(goodwork)).toEqual([["00", "0000"], ["000", "0000"], ["000", "0000"]])
 	})
 })
 
--- src/scripts/timeInBinary.js
+++ src/scripts/timeInBinary.js
@@ -3,12 +3,20 @@ import leftPad from "left-pad"
 
 export default function(time) {
 	if (moment.isMoment(time)) {
-		return [time.hour(), time.minute(), time.second()].map(BCD)
+		let timeInBCD =  [time.hour(), time.minute(), time.second()].map(splitAndPad)
+		timeInBCD[0][0] = timeInBCD[0][0].substring(2)
+		timeInBCD[1][0] = timeInBCD[1][0].substring(1)
+		timeInBCD[2][0] = timeInBCD[2][0].substring(1)
+		return timeInBCD
 	} else {
 		throw new Error("Invalid time!")
 	}
 }
 
-function BCD(time) {
-	return leftPad(time.toString(), 2, "0").split("")
+function splitAndPad(time) {
+	return leftPad(time.toString(), 2, "0").split("").map(BCD)
+}
+
+function BCD(decimal) {
+	return leftPad(parseInt(decimal).toString(2), 4, "0")
 }
