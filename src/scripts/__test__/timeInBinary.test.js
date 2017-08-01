8a5e5e36dbe046fdd3ccdaf3816493d3725484ed
Mathspy <mathspy257@gmail.com>
2017-08-01 22:59:08 +0200

    Double dimensional beauty

--- src/scripts/__test__/timeInBinary.test.js
+++ src/scripts/__test__/timeInBinary.test.js
@@ -7,4 +7,23 @@ describe("timeInBinary", () => {
 		expect(() => timeInBinary()).toThrow()
 		expect(() => timeInBinary(moment())).not.toThrow()
 	})
+
+	test("if given time, returns [hh, mm, ss]", () => {
+		let its5 = moment("05:35:07", "HH:mm:ss")
+		expect(timeInBinary(its5)).toEqual([["0", "5"], ["3", "5"], ["0", "7"]])
+
+		let timegoesfast = moment("23:59:59", "HH:mm:ss")
+		expect(timeInBinary(timegoesfast)).toEqual([["2", "3"], ["5", "9"], ["5", "9"]])
+
+		let goodwork = moment("00:00:00", "HH:mm:ss")
+		expect(timeInBinary(goodwork)).toEqual([["0", "0"], ["0", "0"], ["0", "0"]])
+	})
 })
+
+/*
+To-do-list:
+-timeInBinary:-
+	>seprate hour/minute/second from time
+	>recurrsively transform each of them into two arrays
+
+*/
--- src/scripts/timeInBinary.js
+++ src/scripts/timeInBinary.js
@@ -1,9 +1,14 @@
 import moment from "moment"
+import leftPad from "left-pad"
 
 export default function(time) {
 	if (moment.isMoment(time)) {
-
+		return [time.hour(), time.minute(), time.second()].map(BCD)
 	} else {
 		throw new Error("Invalid time!")
 	}
+}
+
+function BCD(time) {
+	return leftPad(time.toString(), 2, "0").split("")
 }
