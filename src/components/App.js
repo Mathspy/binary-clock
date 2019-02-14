36bf14d72061db2981eb8cb37db841f8bcdcea1a
Mathspy <mathspy257@gmail.com>
2019-02-14 12:40:22 +0200

    Created a popup to notify user of clickability

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 
 import useInterval from "../hooks/useInterval";
 
@@ -7,17 +7,62 @@ import * as dots from "./dots";
 
 const dotNames = Object.keys(dots);
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "changeDot":
+      return {
+        dotIndex: (state.dotIndex + 1) % dotNames.length,
+        popupVisibility: false,
+        popupTimer: false
+      };
+    case "showPopup":
+      return { ...state, popupVisibility: true };
+    default:
+      throw new Error(`${action.type} is not a known action type`);
+  }
+}
+
 const App = () => {
   const [time, setTime] = useState(new Date());
-  const [dotIndex, setDotIndex] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    dotIndex: 0,
+    popupVisibility: false,
+    popupTimer: true
+  });
 
   useInterval(() => {
     setTime(new Date());
   }, 1000);
 
+  useEffect(() => {
+    if (state.popupTimer) {
+      setTimeout(() => dispatch({ type: "showPopup" }), 3000);
+    }
+  }, [state.popupTimer]);
+
   return (
-    <div onClick={() => setDotIndex(() => (dotIndex + 1) % dotNames.length)}>
-      <Clock time={time} Shape={dots[dotNames[dotIndex]]} />
+    <div
+      style={{ height: "100%" }}
+      onClick={() => dispatch({ type: "changeDot" })}
+    >
+      <Clock time={time} Shape={dots[dotNames[state.dotIndex]]} />
+      {state.popupVisibility && (
+        <div
+          style={{
+            position: "absolute",
+            left: "50px",
+            top: "50px",
+            backgroundColor: "black",
+            color: "white",
+            opacity: 0.3,
+            padding: "5px",
+            fontSize: "24px",
+            userSelect: "none"
+          }}
+        >
+          Click Anywhere!
+        </div>
+      )}
     </div>
   );
 };
