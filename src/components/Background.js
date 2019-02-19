0382077f2d32cd67f9e06c2717c949775c217e8d
Mathspy <mathspy257@gmail.com>
2019-02-19 03:24:22 +0200

    Updated favicon, title and theme-color
    - Theme color 🤯 Can't wait to see if this looks good on Android

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,12 +1,19 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
+import { useEffect } from "react";
 import { useSpring, animated } from "react-spring";
 
 import useDarkMode from "../hooks/useDarkMode";
 
 const Background = ({ children, ...props }) => {
   const [isDarkMode] = useDarkMode();
-  const style = useSpring({ background: isDarkMode ? "#131415" : "#fff" });
+
+  const background = isDarkMode ? "#131415" : "#FFF";
+  const style = useSpring({ background });
+
+  useEffect(() => {
+    document.querySelector("meta[name=theme-color]").content = background;
+  }, [background]);
 
   return (
     <animated.div css={{ height: "100%" }} style={style} {...props}>
